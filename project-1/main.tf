489b4d9042144f09d886fc4daf748e9c841c9f4c
darshan.hulswar <dhulswar591@gmail.com>
2025-07-06 14:47:14 +0530

    modified main.tf and added approval stage in Jenkins

--- project-1/main.tf
+++ project-1/main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "main_subnet" {
 }
 
 # Create a private Subnet
-resource "aws_subnet" "main_subnet" {
+resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1b"
@@ -55,11 +55,11 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route" "public_route" {
     route_table_id = aws_route_table.public_route_table.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw
+    gateway_id = aws_internet_gateway.igw.id
 }
 
 # route table association
 resource "aws_route_table_association" "association" {
     subnet_id = aws_subnet.main_subnet.id
-    route_table_id = aws_route_table.public_route.id
+    route_table_id = aws_route_table.public_route_table.id
 }
